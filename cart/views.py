ce6a4367a34e7c3f1f3803a1f816a9c084c0e72f
sheikhxzaheer <sheikhzaheer037@gmail.com>
2023-04-30 15:43:49 +0530

    n

--- cart/views.py
+++ cart/views.py
@@ -3,4 +3,6 @@ from .models import *
 # Create your views here.
 def cart(request):
     cart_items = Cart.objects.filter(user=request.user)
+    for item in cart_items:
+        item.total_price = item.product.product_price * item.quantity
     return render(request, 'cart.html', {'cart_items': cart_items})
