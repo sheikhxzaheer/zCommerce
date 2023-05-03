5df79cecc4456655c8a1747d97f08360927028aa
sheikhxzaheer <sheikhzaheer037@gmail.com>
2023-05-03 16:20:38 +0530

    added add function in cart though json not used ajax

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from cart import views
 
 urlpatterns = [
     path('', views.cart, name='cart'),
-    
+    path('addcart/<int:cart_id>/', views.addCart, name='addCart'),
+
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
+from django.http import JsonResponse
 # Create your views here.
 def cart(request):
     cart_items = Cart.objects.filter(user=request.user)
     for item in cart_items:
         item.total_price = item.product.product_price * item.quantity
     return render(request, 'cart.html', {'cart_items': cart_items})
+
+def addCart(request, cart_id):
+    cart = get_object_or_404(Cart, id=cart_id)
+    cart.quantity += 1
+    cart.save()
+    return JsonResponse({"cart": cart.quantity})
