f455552ab4fed35490eb38285dd9e916e0cb217b
sheikhxzaheer <sheikhzaheer037@gmail.com>
2023-05-09 16:37:58 +0530

    google sign in working properly

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
+   path('signup/', views.signUp, name='signUpPage'),
    path('login/', views.zLogin, name='loginPage'),
    path('logout/', views.logoutPage, name='logoutPage'),
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login as auth_login, logout
+from django.urls import reverse_lazy
 # Create your views here.
+
+
+def signUp(request):
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        user = User.objects.create_user(
+            username=username, first_name=first_name, email=email)
+        user.set_password(password)
+        user.save()
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+        return redirect('/')
+    else:
+        data={
+            "title": "Register",
+        }
+        return render(request, "signup.html", data)
+
+
+
 def zLogin(request):
     if request.method == "POST":
         username = request.POST.get('username')
@@ -18,6 +44,8 @@ def zLogin(request):
         "title": "Log In"
     }
     return render(request, "login.html", data)
+
+
 
 def logoutPage(request):
     logout(request)
