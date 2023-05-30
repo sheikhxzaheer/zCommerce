4f8192d9e057523fe6c0ab77b66563f1155f513b
sheikhxzaheer <sheikhzaheer037@gmail.com>
2023-05-30 10:29:55 +0530

    set up vercel

--- zaheer/settings.py
+++ zaheer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dmm9bhzrvh3cumncqc$vhi49fa&^!f$(v3@#-hvb+-&&l5tsiu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
