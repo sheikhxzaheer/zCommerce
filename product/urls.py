d43e72171ec31f2745886cef1b021026c0e4c32a
sheikhxzaheer <sheikhzaheer037@gmail.com>
2023-06-23 17:57:43 +0530

    add chnage in home page

--- product/urls.py
+++ product/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.Products, name='Product'),
     path('product-details/<product_id>', views.productDetails, name='productDetails'),
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-    
     path('searchproducts/', views.search_products, name='searchproducts'),
 ]
