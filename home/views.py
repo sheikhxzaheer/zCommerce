a2c5348448a517d5ad08794c9712b2b0564d1eaa
sheikhxzaheer <sheikhzaheer037@gmail.com>
2023-06-24 23:16:22 +0530

    added loop to home product section

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
+from product.models import Product
 
 # Create your views here.
 def Home(request):
-    return render(request, "index.html")
-    
+    homeProduct = Product.objects.all()
+    return render(request, "index.html", {'homeProduct': homeProduct})
+
