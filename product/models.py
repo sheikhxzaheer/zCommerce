9126525264724f0eb5a735017cc14a2e908289b7
sheikhxzaheer <sheikhzaheer037@gmail.com>
2023-08-18 11:15:15 +0530

    chnged img url

--- product/models.py
+++ product/models.py
@@ -4,7 +4,7 @@ from tinymce.models import HTMLField
 # Create your models here.
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
-    product_image = models.FileField(upload_to="products/images", default='')
+    product_image = models.URLField(blank=True)
     product_title = models.CharField(max_length=50, default='')
     product_price = models.IntegerField(default=0, null=True, blank=True)
     product_offer_price = models.IntegerField(default=0, null=True, blank=True)
